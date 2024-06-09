4824ab6c2d17915c2710a54a2c4677385b3d2b60
Curious-MX <max536854@gmail.com>
2024-06-09 22:27:21 +0330

    Styles, Attributes and classes

--- js/script.js
+++ js/script.js
@@ -32,19 +32,19 @@ document.addEventListener("keydown", function (e) {
 ///////////////////////////////////////
 
 // Selecting elements
-console.log(document.documentElement);
-console.log(document.head);
-console.log(document.body);
+// console.log(document.documentElement);
+// console.log(document.head);
+// console.log(document.body);
 
 const header = document.querySelector(".header");
 const allSections = document.querySelector(".section");
-console.log(allSections);
+// console.log(allSections);
 
 document.getElementById("section--1");
 const allButtons = document.getElementsByTagName("button");
-console.log(allButtons);
+// console.log(allButtons);
 
-console.log(document.getElementsByClassName("btn"));
+// console.log(document.getElementsByClassName("btn"));
 
 // Creating and inserting elements
 const message = document.createElement("div");
@@ -56,13 +56,50 @@ message.innerHTML =
 // header.append(message);
 // header.append(message.cloneNode(true));
 
+// header.after(message);
 header.before(message);
-header.after(message);
 
 // Delete elements
 document
-.querySelector('.btn--close-cookie')
-.addEventListener('click', function() {
-  // message.remove()
-  message.parentElement.removeChild(message)
-})
+  .querySelector(".btn--close-cookie")
+  .addEventListener("click", function () {
+    // message.remove()
+    message.parentElement.removeChild(message);
+  });
+
+// Styles
+message.style.backgroundColor = "#37383d";
+message.style.width = "120%";
+
+console.log(message.style.height);
+console.log(message.style.backgroundColor);
+
+message.style.height =
+  Number.parseFloat(getComputedStyle(message).height, 10) + 30 + "px";
+
+document.documentElement.style.setProperty("--color-primary", "orangered");
+
+// Attributes
+const logo = document.querySelector(".nav__logo");
+console.log(logo.alt);
+console.log(logo.className);
+
+// Non-standard
+console.log(logo.designer);
+console.log(logo.getAttribute("designer"));
+
+console.log(logo.src);
+console.log(logo.getAttribute("src"));
+
+const link = document.querySelector(".nav__link--btn");
+console.log(link.href);
+console.log(link.getAttribute("href"));
+
+// Classes
+logo.classList.add("c", "j");
+logo.classList.remove("c", "j");
+logo.classList.toggle("c");
+logo.classList.contais("c"); // not includes
+
+// Don't use
+logo.className = "Max";
