8e550a2323738c440a29d7f0ae93fc55efa297ef
Curious-MX <max536854@gmail.com>
2024-06-13 19:56:11 +0330

    Event Propagation in practice

--- js/script.js
+++ js/script.js
@@ -36,70 +36,137 @@ document.addEventListener("keydown", function (e) {
 // console.log(document.head);
 // console.log(document.body);
 
-const header = document.querySelector(".header");
-const allSections = document.querySelector(".section");
-// console.log(allSections);
+// const header = document.querySelector(".header");
+// const allSections = document.querySelector(".section");
+// // console.log(allSections);
 
-document.getElementById("section--1");
-const allButtons = document.getElementsByTagName("button");
-// console.log(allButtons);
+// document.getElementById("section--1");
+// const allButtons = document.getElementsByTagName("button");
+// // console.log(allButtons);
 
-// console.log(document.getElementsByClassName("btn"));
+// // console.log(document.getElementsByClassName("btn"));
 
-// Creating and inserting elements
-const message = document.createElement("div");
-message.classList.add("cookie-message");
-// message.textContent = 'We use cookied for improved functionality and analytics.';
-message.innerHTML =
-  ' we use cookeid for improved functionality and analytics. <button class="btn btn--close-cookie"> Got it!</button>';
-// header.prepend(message);
-// header.append(message);
-// header.append(message.cloneNode(true));
+// // Creating and inserting elements
+// const message = document.createElement("div");
+// message.classList.add("cookie-message");
+// // message.textContent = 'We use cookied for improved functionality and analytics.';
+// message.innerHTML =
+//   ' we use cookeid for improved functionality and analytics. <button class="btn btn--close-cookie"> Got it!</button>';
+// // header.prepend(message);
+// // header.append(message);
+// // header.append(message.cloneNode(true));
 
-// header.after(message);
-header.before(message);
+// // header.after(message);
+// header.before(message);
 
-// Delete elements
-document
-  .querySelector(".btn--close-cookie")
-  .addEventListener("click", function () {
-    // message.remove()
-    message.parentElement.removeChild(message);
-  });
+// // Delete elements
+// document
+//   .querySelector(".btn--close-cookie")
+//   .addEventListener("click", function () {
+//     // message.remove()
+//     message.parentElement.removeChild(message);
+//   });
 
-// Styles
-message.style.backgroundColor = "#37383d";
-message.style.width = "120%";
+// // Styles
+// message.style.backgroundColor = "#37383d";
+// message.style.width = "120%";
 
-console.log(message.style.height);
-console.log(message.style.backgroundColor);
+// console.log(message.style.height);
+// console.log(message.style.backgroundColor);
 
-message.style.height =
-  Number.parseFloat(getComputedStyle(message).height, 10) + 30 + "px";
+// message.style.height =
+//   Number.parseFloat(getComputedStyle(message).height, 10) + 30 + "px";
 
-document.documentElement.style.setProperty("--color-primary", "orangered");
+// document.documentElement.style.setProperty("--color-primary", "orangered");
 
-// Attributes
-const logo = document.querySelector(".nav__logo");
-console.log(logo.alt);
-console.log(logo.className);
+// // Attributes
+// const logo = document.querySelector(".nav__logo");
+// console.log(logo.alt);
+// console.log(logo.className);
 
-// Non-standard
-console.log(logo.designer);
-console.log(logo.getAttribute("designer"));
+// // Non-standard
+// console.log(logo.designer);
+// console.log(logo.getAttribute("designer"));
 
-console.log(logo.src);
-console.log(logo.getAttribute("src"));
+// console.log(logo.src);
+// console.log(logo.getAttribute("src"));
 
-const link = document.querySelector(".nav__link--btn");
-console.log(link.href);
-console.log(link.getAttribute("href"));
+// const link = document.querySelector(".nav__link--btn");
+// console.log(link.href);
+// console.log(link.getAttribute("href"));
 
-// Classes
-logo.classList.add("c", "j");
-logo.classList.remove("c", "j");
-logo.classList.toggle("c");
-logo.classList.contains("c"); // not includes
+// // Classes
+// logo.classList.add("c", "j");
+// logo.classList.remove("c", "j");
+// logo.classList.toggle("c");
+// logo.classList.contains("c"); // not includes
 
-// Don't use
-logo.className = "Max";
+// // Don't use
+// logo.className = "Max";
+
+const btnScrollTo = document.querySelector(".btn--scroll-to");
+const sectional = document.querySelector("#section--1");
+
+btnScrollTo.addEventListener("click", function (e) {
+  const s1coords = sectional.getBoundingClientRect();
+  console.log(s1coords);
+
+  console.log(e.target.getBoundingClientRect());
+
+  console.log("Current scroll (X/Y)", window.pageXOffset, window.pageYOffset);
+
+  console.log(
+    "height/width viewport",
+    document.documentElement.clientWidth,
+    document.documentElement.clientHeight
+  );
+
+  // Scrolling
+  // window.scrollTo(
+  //   s1coords.left + window.pageXOffset,
+  //   s1coords.top + window.pageYOffset
+  // );
+
+  // window.scrollTo({
+  //   left: s1coords.left + window.pageXOffset,
+  //   top: s1coords.top + window.pageYOffset,
+  //   behavior: 'smooth'
+  // })
+
+  sectional.scrollIntoView({ behavior: "smooth" });
+});
+
+// const h1 = document.querySelector("h1");
+
+// const alertH1 = function (e) {
+//   alert("addEventListener: Great! You are reading the heading :D");
+// };
+
+// h1.addEventListener("mouseenter", alertH1);
+
+// setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 3000);
+
+// h1.onmouseenter = function (e) {
+//   alert("onmouseenter: Great! You are reading the heading :D");
+// };
+
+// rgb(255,255,255);
+const randomInt = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1) + min);
+const randomColor = () =>
+  `rgb(${randomInt(0, 255)}, ${randomInt(0, 255)}, ${randomInt(0, 255)})`;
+
+document.querySelector(".nav__link").addEventListener("click", function (e) {
+  this.style.backgroundColor = randomColor();
+  e.stopPropagation()
+});
+
+document.querySelector(".nav__links").addEventListener("click", function (e) {
+  this.style.backgroundColor = randomColor()
+  e.stopPropagation()
+});
+
+document.querySelector(".nav").addEventListener("click", function (e) {
+  this.style.backgroundColor = randomColor();
+  e.stopPropagation()
+});
